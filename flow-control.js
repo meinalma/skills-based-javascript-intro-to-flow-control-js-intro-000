aaaaf059b2f5588a4111e5f6fec096081f124dce
Dalma Lipcsei <lipcsei.dalma@gmail.com>
2019-07-11 19:43:34 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -41,6 +41,15 @@ function switchAge(age) {
     case 16:
       console.log("You are a teenager");
       break;
+    case 17:
+      console.log("You are a teenager");
+      break;
+    case 18:
+      console.log("You are a teenager");
+      break;
+    case 19:
+      console.log("You are a teenager");
+      break;
     default:
        console.log("You have an age");
   }
