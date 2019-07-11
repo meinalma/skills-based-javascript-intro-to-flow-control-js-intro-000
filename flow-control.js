5693281b454de40ace46b053a90527883578a30e
Dalma Lipcsei <lipcsei.dalma@gmail.com>
2019-07-11 18:49:22 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,43 @@
 function basicTeenager(age) {
-
+  if (13 < age < 19) {
+    console.log("You are a teenager!");
+  } else {
+    return undefined;
+  }
 }
 
 function teenager(age) {
+  if (13 < age < 19) {
+    console.log("You are a teenager!");
+  } else {
+    console.log("You are not a teenager");
+  }
 
 }
 
 function ageChecker(age) {
-
+  if (13 < age < 19) {
+    return "You are a teenager!";
+  } else if (age <= 12) {
+    return "You are a kid";
+  }
+    return "You are a grownup";
 }
 
 function ternaryTeenager(age) {
-
+  return  13 < age < 19 ?
+  "You are a teenager" :
+  "You are not a teenager";
 }
 
 function switchAge(age) {
+  switch(age) {
+    case 13 <= age <= 19:
+      console.log("Dance to Pharell's 'Happy'");
+      break;
+    default:
+      console.log("You have an age");
+      
+  }
 
 }
