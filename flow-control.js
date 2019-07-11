d24fbf4d1a8d3e354747c615f21cf16524948d8e
Dalma Lipcsei <lipcsei.dalma@gmail.com>
2019-07-11 19:27:31 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -6,9 +6,9 @@ function basicTeenager(age) {
 
 function teenager(age) {
   if (13 <= age && age <= 19) {
-    console.log("You are a teenager!");
+    return "You are a teenager!";
   } else {
-    console.log("You are not a teenager");
+    return "You are not a teenager";
   }
 
 }
